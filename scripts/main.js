b8d1947b643a95562ff22b27c372a71b9e9e0b80
Diogo <71763076+GeneralCoruja@users.noreply.github.com>
2025-02-24 15:29:21 +0100

    update mobile support

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,40 @@
 const DEFAULT_BRUSH_COLOR = '#000000';
-var lastMousePos = null; 
+var lastMousePos = null;
+var isMobile = false;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        isMobile = true;
+    }
 }
 
 function draw() {
+    pencilDraw();
+}
+
+function pencilDraw() {
     stroke(DEFAULT_BRUSH_COLOR);
     strokeWeight(10);
-    
-    if(mouseIsPressed){
+    if (mouseIsPressed) {
         point(mouseX, mouseY);
-        if(lastMousePos != null){
+        if (lastMousePos != null) {
             line(lastMousePos[0], lastMousePos[1], mouseX, mouseY);
         }
         lastMousePos = [mouseX, mouseY];
     }
 }
 
+
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
 }
 
 
-function mouseReleased(){
+function mouseReleased() {
+    lastMousePos = null;
+}
+
+function touchEnded(){
     lastMousePos = null;
 }
